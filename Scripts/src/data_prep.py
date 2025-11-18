00fe3955d93eb9bdd614b71fa09af2a02e99fac6
conrad <ctestagrose95@gmail.com>
2025-11-17 20:03:42 -0500

    Included Code to train on single GPU and MPS. Provided README file with outline on how to run.

--- Scripts/src/data_prep.py
+++ Scripts/src/data_prep.py
@@ -37,10 +37,6 @@ class DataPreparer:
             
             targets = split_dict
 
-        # Keep Track of resistant and susceptible for debugging purposes
-        susceptible = 0
-        resistant = 0
-
         # Iterate through the fasta files
         for fasta_file in tqdm(fasta_files, desc=f"Processing FASTA files for {target_format}...", unit="files"):
             # Get isolate number
@@ -49,37 +45,11 @@ class DataPreparer:
                 isolate = isolate.split("_")[0]
                 target_format = self.args.antibiotic
             if isolate in targets:
-                # Handle multi-category labels
-                if target_format == 'multi-cat':
-                    ordering = ["AMI", "INH", "RIF", "LEV", "ETH",
-                                "EMB", "RFB", "MXF", "KAN"]
-                    label_dict = targets[isolate]
-                    label = []
-                    for item in ordering:
-                        label.append(label_dict[item])
-                elif target_format == 'multi-cat rare':
-                    ordering = ["LZD", "BDQ", "DLM", "CFZ"]
-                    label_dict = targets[isolate]
-                    label = []
-                    for item in ordering:
-                        label.append(label_dict[item])
-                elif target_format == 'multi-cat all':
-                    ordering = ["AMI", "INH", "RIF", "LEV", "ETH",
-                                "EMB", "RFB", "MXF", "KAN", "LZD",
-                                "BDQ", "DLM", "CFZ"]
-                    label_dict = targets[isolate]
-                    label = []
-                    for item in ordering:
-                        label.append(label_dict[item])
-                else:
-                    # Single antibiotic case
-                    label = targets[isolate][target_format]
+                label = targets[isolate][target_format]
                 
                 # Check for valid labels (ignoring isolates with NaN values in any label)
-                if (('multi-cat' in target_format and not any(math.isnan(x) for x in label)) or
-                        ('multi-cat' not in target_format and not math.isnan(label))):
-                    clean_sequence, genes_in_isolate = self.gene_manager.read_fasta_file_genes(fasta_file,
-                                                                                                   target_format)
+                if target_format and not math.isnan(label):
+                    clean_sequence, genes_in_isolate = self.gene_manager.read_fasta_file_genes(fasta_file, target_format)
         
                     # If there is a sequence, append; else, print no valid sequences
                     if clean_sequence:
@@ -87,15 +57,7 @@ class DataPreparer:
                             if "<P>" in clean_sequence:
                                 print(clean_sequence)
                         sequences.append(clean_sequence)
-        
-                        # Increment counts based on susceptibility/resistance for binary labels (single antibiotic)
-                        if 'multi-cat' in target_format:
-                            if str(label) == "0.0":
-                                susceptible += 1
-                            if str(label) == "1.0":
-                                resistant += 1
-        
-                        # Append the multi-category or binary label
+
                         labels.append(label)
                         seq_ids.append(isolate)
                         seq_id_counter += 1
